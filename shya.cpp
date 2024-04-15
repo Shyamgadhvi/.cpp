e7710b40650df29c2e76a06ed6be0ea680d45694
Shyamgadhvi <162123491+Shyamgadhvi@users.noreply.github.com>
2024-04-15 23:44:56 +0530

    Add files via upload

---
+++ shya.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+ using namespace std;
+ int main()
+
+ { 
+ int radius = 4;
+ const float PI = 3.14;
+ double area ;
+ 
+ area = PI *radius* radius ;
+ cout << "area of circul"<< area ;
+    return 0; 
+ }
